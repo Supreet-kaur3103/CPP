7fbb87510d7d8bfabecdb2e82f0bf9409600d543
Supreet Kaur <supreetkaur081097@gmail.com>
2023-07-07 03:59:36 +0100

    Introduction

---
+++ Tutorials/1introduction.cpp
@@ -0,0 +1,13 @@
+/*C++ is one of the most popular programming languages.
+It is used to build games, operating systems, browsers, and much more!
+The biggest advantage of C++ is its scalability and control over how it uses resources.
+The following line of code displays "Welcome to C++" on the screen as an output:*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "Welcome to C++";
+    return 0;
+}
+
